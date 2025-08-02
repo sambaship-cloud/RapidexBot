6273bce7cc4fb572d384f08da3062d263b2ec0a7
csma118 <csma118@wgu.edu>
2025-08-02 05:06:40 -1000

    export function test

--- api/webhook.js
+++ api/webhook.js
@@ -1,23 +1,3 @@
-import { MessagingResponse } from 'twilio';
-import { buffer } from 'micro';
-
-export const config = {
-  api: {
-    bodyParser: false,
-  },
-};
-
-export default async function handler(req, res) {
-  const buf = await buffer(req);
-  const params = new URLSearchParams(buf.toString());
-  const incomingMsg = params.get('Body')?.trim();
-
-  const twiml = new MessagingResponse();
-  const reply = incomingMsg
-    ? `You said: "${incomingMsg}"`
-    : "Please send a message.";
-
-  twiml.message(reply);
-  res.setHeader('Content-Type', 'text/xml');
-  res.status(200).send(twiml.toString());
+export default function handler(req, res) {
+  res.status(200).send('Webhook is live!');
 }
