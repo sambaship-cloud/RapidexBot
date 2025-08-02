2e762893db68e3bc73a7ecce9f4992bb2baba616
csma118 <csma118@wgu.edu>
2025-08-02 05:25:55 -1000

    starting over

--- api/webhook.js
+++ api/webhook.js
@@ -1,12 +1,23 @@
+const { MessagingResponse } = require('twilio');
+const { buffer } = require('micro');
+
 exports.config = {
   api: {
     bodyParser: false,
   },
 };
 
-module.exports = (req, res) => {
+module.exports = async function handler(req, res) {
   if (req.method !== 'POST') {
     return res.status(405).send('Method Not Allowed');
   }
-  res.status(200).send('Minimal webhook works!');
-};
+
+  try {
+    const buf = await buffer(req);
+    const params = new URLSearchParams(buf.toString());
+    const incomingMsg = params.get('Body')?.trim();
+
+    const twiml = new MessagingResponse();
+    const reply = incomingMsg
+      ? `You said: "${incomingMsg}"`
+      : 'Plea
