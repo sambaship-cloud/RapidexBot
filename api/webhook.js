64f6dce77e9f676885f0d2aa4884c7a43c8ddb5d
csma118 <csma118@wgu.edu>
2025-08-02 05:20:30 -1000

    werbhook update to common

--- api/webhook.js
+++ api/webhook.js
@@ -1,13 +1,13 @@
-import { MessagingResponse } from 'twilio';
-import { buffer } from 'micro';
+const { MessagingResponse } = require('twilio');
+const { buffer } = require('micro');
 
-export const config = {
+exports.config = {
   api: {
     bodyParser: false,
   },
 };
 
-export default async function handler(req, res) {
+module.exports = async function handler(req, res) {
   if (req.method !== 'POST') {
     return res.status(405).send('Method Not Allowed');
   }
@@ -29,4 +29,4 @@ export default async function handler(req, res) {
     console.error('Error processing webhook:', error);
     res.status(500).send('Internal Server Error');
   }
-}
+};
